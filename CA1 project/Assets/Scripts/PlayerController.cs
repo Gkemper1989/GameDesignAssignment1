a4fe5cedf2d9642ace49b67e8519e1a011a07662
Gkemper1989 <g.kemperweber@gmail.com>
2021-02-26 13:49:46 +0000

    First Commit
    Basics player controller and spawn manager set up

---
+++ CA1 project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variables
+    [SerializeField] float speed = 10.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.down * speed * Time.deltaTime * horizontalInput);
+        transform.Translate(Vector2.right * speed * Time.deltaTime * verticalInput);
+    }
+}
---
+++ CA1 project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //Variables
+    [SerializeField] GameObject[] asteroidPrefabs;
+    private float spawnRangeX = 7.0f;
+    private float spawnHeight = 6.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calling the spawn method with a start delay and a repeat time scope
+        InvokeRepeating("SpawnRandomAsteroids", startDelay, spawnInterval);
+    }
+
+    //Spawning random asteroids from the array in random positions 
+    void SpawnRandomAsteroids()
+    {
+        int asteroidIndex = Random.Range(0, asteroidPrefabs.Length);
+        Vector2 spawnPosition = new Vector2(Random.Range(-spawnRangeX, spawnRangeX), spawnHeight);
+
+        Instantiate(asteroidPrefabs[asteroidIndex], spawnPosition, Quaternion.identity);
+    }
+}
